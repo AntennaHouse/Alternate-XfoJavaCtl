0091ce6d3658b42493fd7b4732df7c53acf2fcbe
Angelo Cano <acano@fastmail.fm>
2021-04-30 15:41:13 -0400

    Formatter 7.2 enviornment variables

--- src/jp/co/antenna/XfoJavaCtl/XfoObj.java
+++ src/jp/co/antenna/XfoJavaCtl/XfoObj.java
@@ -23,6 +23,7 @@ public class XfoObj {
     public static final int EST_STDOUT = 1;
     public static final int EST_STDERR = 2;
     private static final String[] AH_HOME_ENV = {
+	"AHF72_64_HOME", "AHF72_HOME",
 	"AHF71_64_HOME", "AHF71_HOME",
 	"AHF70_64_HOME", "AHF70_HOME",
 	"AHF64_64_HOME", "AHF64_HOME",
