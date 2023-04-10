ac0c8e07a7c9bc25ac46a225261d4acb5a1db4a2
brugal <angelo@antennahouse.com>
2023-04-10 14:21:32 -0400

    Formatter 7.3 HOME environment variables

--- src/jp/co/antenna/XfoJavaCtl/XfoObj.java
+++ src/jp/co/antenna/XfoJavaCtl/XfoObj.java
@@ -23,6 +23,7 @@ public class XfoObj {
     public static final int EST_STDOUT = 1;
     public static final int EST_STDERR = 2;
     private static final String[] AH_HOME_ENV = {
+	"AHF73_64_HOME", "AHF73_32_HOME",
 	"AHF72_64_HOME", "AHF72_HOME",
 	"AHF71_64_HOME", "AHF71_HOME",
 	"AHF70_64_HOME", "AHF70_HOME",
